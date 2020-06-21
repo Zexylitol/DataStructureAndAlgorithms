6565934c53887ed896d3af4709922296791fabd1
Zexylitol <1398274297@qq.com>
2020-06-21 18:59:46 +0800

    add Algorithms/BinarySearch

--- src/Algorithms/BinarySearch/BinarySearch.java
+++ src/Algorithms/BinarySearch/BinarySearch.java
@@ -84,6 +84,7 @@ public class BinarySearch {
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
+        System.out.println();
 
         BinarySearch bs = new BinarySearch();
 
