013efcaefe12aa5de207607260f41ce3afdde32c
Zexylitol <1398274297@qq.com>
2020-07-12 19:14:49 +0800

    Update MergeSort.java

--- src/Algorithms/Sort/MergeSort.java
+++ src/Algorithms/Sort/MergeSort.java
@@ -1,97 +1,118 @@
 package Algorithms.Sort;
 
-
-import DataStructure.Array.Array;
-
 /**
  * 归并排序
- * @author yzz
- *
+ * 优化 1：在「小区间」里转向使用「插入排序」，Java 源码里面也有类似这种操作，「小区间」的长度是个超参数，需要测试决定，这里参考了 JDK 源码；
+ * 优化 2： 在「两个数组」本身就是有序的情况下，无需合并；
+ * 优化 3：全程使用一份临时数组进行「合并两个有序数组」的操作，避免创建临时数组和销毁的消耗，避免计算下标偏移量
+ * 时间复杂度：O(NlogN)，这里 N 是数组的长度；
+ * 空间复杂度：O(N)，辅助数组与输入数组规模相当；
+ * @create 2020-07-12 19:04
  */
 public class MergeSort {
-	
-	public Array<Integer> mergeSort(Array<Integer> array) {
-		// 递归终止条件
-		if (array.getSize() <= 1) {
-			return array;
-		}
-		
-		int mid = array.getSize() / 2;
-		// 左半部分进行拆分
-		Array<Integer> leftArray = new Array<Integer>(mid);
-		Array<Integer> rightArray = new Array<Integer>(mid);
-		for (int i = 0; i < array.getSize(); i++) {
-			if (i < mid) {
-				leftArray.add(i, array.get(i));
-			} else {
-				rightArray.add(i-mid, array.get(i));
-			}
-		}
-		
-		leftArray = mergeSort(leftArray);
-		rightArray = mergeSort(rightArray);
-		
-		System.out.print("左： ");
-		for(int a = 0; a < mid; a++) {
-			System.out.print(leftArray.get(a) + " " + a + " ");
-		}
-		System.out.print("右： ");
-		for(int a = 0; a < mid; a++) {
-			System.out.print(rightArray.get(a) + " ");
-		}
-		System.out.println();
-		
-		// 递归结束后
-		int leftIndex = 0;
-		int rightIndex = 0;
-		// 进行合并
-		Array<Integer> results = new Array<Integer>(2 * mid);
-		int cnt = 0;
-		while (leftIndex < mid && rightIndex < mid) {
-			if (leftArray.get(leftIndex) < rightArray.get(rightIndex)) {
-				results.add(cnt, leftArray.get(leftIndex));
-				leftIndex++;
-				cnt++;
-			} else {
-				results.add(cnt, rightArray.get(rightIndex));
-				rightIndex++;
-				cnt++;
-			}
-		}
-		
-		if (leftIndex < mid) {    // 把左边剩余元素加到排序结果中
-			for (int t = leftIndex; t < mid; t++) {
-				results.add(cnt++, leftArray.get(t));
-			}			
-		} 
-		if (rightIndex < mid) {      // 把右边剩余元素加到排序结果中
-			for (int t = rightIndex; t < mid; t++) {
-				results.add(cnt++, rightArray.get(t));
-			}	
-		}
-		
-		return results;
-		
-	}
+    /**
+     * 列表大小等于或小于该大小，将优先于 mergeSort 使用插入排序
+     */
+    private static final int INSERTION_SORT_THRESHOLD = 7;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		MergeSort ms = new MergeSort();
-		
-		Array<Integer> arr = new Array<Integer>(7);
-		arr.addFirst(8);
-		arr.addFirst(1);
-		arr.addFirst(4);
-		arr.addFirst(6);
-		arr.addFirst(2);
-		arr.addFirst(3);
-		arr.addFirst(5);
-		arr.addFirst(7);
-		
-		System.out.println("排序前： " + arr.toString());
-		arr = ms.mergeSort(arr);
-		System.out.println("排序后： " + arr.toString());
-	}
+    public int[] sortArray(int[] nums) {
+        int len = nums.length;
+        int[] temp = new int[len];
+        mergeSort(nums, 0, len - 1, temp);
+        return nums;
+    }
+
+    /**
+     * 对数组 nums 的子区间 [left, right] 进行归并排序
+     *
+     * @param nums
+     * @param left
+     * @param right
+     * @param temp  用于合并两个有序数组的辅助数组，全局使用一份，避免多次创建和销毁
+     */
+    public void mergeSort(int[] nums, int left, int right, int[] temp) {
+        // 小区间使用插入排序
+        if (right - left <= INSERTION_SORT_THRESHOLD) {
+            insertionSort(nums, left, right);
+            return;
+        }
+
+        int mid = left + (right - left) / 2;
+        // Java 里有更优的写法，在 left 和 right 都是大整数时，即使溢出，结论依然正确
+        // int mid = (left + right) >>> 1;
+
+        mergeSort(nums, left, mid, temp);
+        mergeSort(nums, mid + 1, right, temp);
+        // 如果数组的这个子区间本身有序，无需合并
+        if (nums[mid] <= nums[mid + 1]) {
+            return;
+        }
+        mergeOfTwoSortedArray(nums, left, mid, right, temp);
+    }
+
+    /**
+     * 对数组 arr 的子区间 [left, right] 使用插入排序
+     *
+     * @param arr   给定数组
+     * @param left  左边界，能取到
+     * @param right 右边界，能取到
+     */
+    private void insertionSort(int[] arr, int left, int right) {
+        for (int i = left + 1; i <= right; i++) {
+            int temp = arr[i];
+            int j = i;
+            while (j > left && arr[j - 1] > temp) {
+                arr[j] = arr[j - 1];
+                j--;
+            }
+            arr[j] = temp;
+        }
+    }
+
+    /**
+     * 合并两个有序数组：先把值复制到临时数组，再合并回去
+     *
+     * @param nums
+     * @param left
+     * @param mid   [left, mid] 有序，[mid + 1, right] 有序
+     * @param right
+     * @param temp  全局使用的临时数组
+     */
+    private void mergeOfTwoSortedArray(int[] nums, int left, int mid, int right, int[] temp) {
+        System.arraycopy(nums, left, temp, left, right + 1 - left);
+
+        int i = left;
+        int j = mid + 1;
+
+        for (int k = left; k <= right; k++) {
+            if (i == mid + 1) {
+                nums[k] = temp[j];
+                j++;
+            } else if (j == right + 1) {
+                nums[k] = temp[i];
+                i++;
+            } else if (temp[i] <= temp[j]) {
+                // 注意写成 < 就丢失了稳定性（相同元素原来靠前的排序以后依然靠前）
+                nums[k] = temp[i];
+                i++;
+            } else {
+                // temp[i] > temp[j]
+                nums[k] = temp[j];
+                j++;
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{5,7,3,1,4,6,2,8};
+        MergeSort ms = new MergeSort();
+        int[] temp = new int[nums.length];
+        ms.mergeSort(nums,0,nums.length-1, temp);
+        System.out.println("排序后：");
+        for(int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
 
 }
