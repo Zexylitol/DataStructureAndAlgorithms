e85eb62354e7138dea0b8d1e91de06220a75e4bd
Zexylitol <1398274297@qq.com>
2020-09-13 17:55:09 +0800

    add comment

--- src/DataStructure/NewGraph/UndirGraph/Graph.java
+++ src/DataStructure/NewGraph/UndirGraph/Graph.java
@@ -5,9 +5,14 @@ package DataStructure.NewGraph.UndirGraph;
  * @create 2020-09-13 15:47
  */
 public interface Graph {
+    // 顶点数
     public int V();
+    // 边数
     public int E();
+    // 顶点v和w之间是否存在边
     public boolean hasEdge(int v, int w);
+    // 返回和v相邻的顶点
     public Iterable<Integer> adj(int v);
+    // 返回顶点v的度
     public int degree(int v);
 }
