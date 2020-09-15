610b32d170c40ecea27bb379b1b232495b1d83ae
Zexylitol <1398274297@qq.com>
2020-09-15 10:02:53 +0800

    检测无向图中的环

---
+++ src/Algorithms/NewGraph/UndirGraph/CycleDetection.java
@@ -0,0 +1,58 @@
+package Algorithms.NewGraph.UndirGraph;
+
+import DataStructure.NewGraph.UndirGraph.AdjSet;
+import DataStructure.NewGraph.UndirGraph.Graph;
+
+/**
+ * 检测无向图中的环
+ **/
+public class CycleDetection {
+
+    private Graph G;
+    private boolean[] visited;
+    private boolean hasCycle = false;
+
+    public CycleDetection(Graph G){
+
+        this.G = G;
+        visited = new boolean[G.V()];
+		// 在每一个连通分量中都进行环检测
+        for(int v = 0; v < G.V(); v ++)
+            if(!visited[v])
+                if(dfs(v, v)){
+                    hasCycle = true;
+                    break;
+                }
+    }
+
+    // 从顶点 v 开始，判断图中是否有环
+	// parent：v的上一个节点
+    private boolean dfs(int v, int parent){
+
+        visited[v] = true;
+        for(int w: G.adj(v))
+            if(!visited[w]){
+                if(dfs(w, v)) return true;
+            }
+            else if(w != parent)      // w是一个已经访问过的节点且不是v的上一个节点，因此图中存在环
+                return true;
+        return false;
+    }
+
+    public boolean hasCycle(){
+        return hasCycle;
+    }
+
+    public static void main(String[] args){
+
+        String filePath = "E:\\Java\\javaWorkSpace\\DataStructure\\src\\DataStructure\\NewGraph\\g.txt";
+        Graph g = new AdjSet(filePath);
+        CycleDetection cycleDetection = new CycleDetection(g);
+        System.out.println(cycleDetection.hasCycle());
+
+        String filePath2 = "E:\\Java\\javaWorkSpace\\DataStructure\\src\\Algorithms\\NewGraph\\g2.txt";
+        Graph g2 = new AdjSet(filePath2);
+        CycleDetection cycleDetection2 = new CycleDetection(g2);
+        System.out.println(cycleDetection2.hasCycle());
+    }
+}
