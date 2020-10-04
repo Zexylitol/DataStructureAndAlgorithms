750a0490de7e1b2e16c56d0a3d8797ff2922121f
Zexylitol <1398274297@qq.com>
2020-10-04 11:27:39 +0800

    自行编写String相关方法:trim,reverse

---
+++ src/Algorithms/String/MyString.java
@@ -0,0 +1,98 @@
+package Algorithms.String;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 自己实现字符串常用方法
+ * @author yzz
+ * @create 2020-10-04 11:15
+ */
+public class MyString {
+    /**
+     * 删除字符串中的多余空格
+     * "  Bob    Loves Alice  " -> "Bob Loves Alice"
+     * @param s
+     * @return
+     */
+    public StringBuilder trimSpaces(String s) {
+        int left = 0, right = s.length()-1;
+        // 去掉字符串开头的空白字符 left<=right 避免越界
+        while (left <= right && s.charAt(left) == ' ') left++;
+        // 去掉字符串末尾的空白字符
+        while (left <= right && s.charAt(right) == ' ') right--;
+
+        // 将字符串间多余的空白字符去除
+        StringBuilder sb = new StringBuilder();
+        while (left <= right) {
+            char c = s.charAt(left);
+            if (c != ' ') {
+                sb.append(c);
+            } else if (sb.charAt(sb.length() - 1) != ' ') {
+                sb.append(c);                     // 空格
+            }
+            left++;
+        }
+        return sb;
+    }
+
+    /**
+     *  将区间[left, right]之间的字符串进行翻转
+     *  "Bob Loves Alice" -> "ecilA sevoL boB"
+     * @param sb
+     * @param left
+     * @param right
+     */
+    public void reverse(StringBuilder sb, int left, int right) {
+        while (left < right) {
+            char tmp = sb.charAt(left);
+            sb.setCharAt(left++, sb.charAt(right));
+            sb.setCharAt(right--, tmp);
+        }
+    }
+
+    /**
+     * 翻转字符串中的每个单词
+     * "ecilA sevoL boB" -> "Alice Loves Bob"
+     * @param sb
+     */
+    public void reverseEachWord(StringBuilder sb) {
+        int n = sb.length();
+        int start = 0, end = 0;
+        while (start < n) {
+            // 循环至单词的末尾 空格结尾
+            while (end < n && sb.charAt(end) != ' ') {
+                end++;
+            }
+            // 翻转单词
+            reverse(sb, start, end-1);
+            // 更新start, 去找下一个单词
+            start = end + 1;
+            end++;
+        }
+    }
+
+    public static void main(String[] args) {
+        // 翻转字符串里的单词
+        String s = "  Bob    Loves Alice  ";
+        // 内置API
+        // 除去开头和末尾的空白字符
+        s = s.trim();
+        // 正则匹配连续的空白字符作为分隔符分割
+        List<String> wordList = Arrays.asList(s.split("\\s+"));
+        Collections.reverse(wordList);
+        System.out.println(String.join(" ", wordList));
+
+        // 自行编写对应函数
+        MyString myString = new MyString();
+        StringBuilder sb = myString.trimSpaces(s);
+
+        // 翻转字符串
+        myString.reverse(sb, 0, sb.length() - 1);
+
+        // 翻转每个单词
+        myString.reverseEachWord(sb);
+        System.out.println(sb.toString());
+    }
+}
